5692597a496c18da11b33e880cc7ee62bc4cc48a
Darmin Parmar <darmin.parmar@ARTOONMM0029s-iMac.local>
2023-03-03 12:14:56 +0530

    Camera setting done

--- Assets/PhotonProject/Script/PlayerSetUp.cs
+++ Assets/PhotonProject/Script/PlayerSetUp.cs
@@ -16,14 +16,13 @@ public class PlayerSetUp : MonoBehaviourPunCallbacks
         {
             myPlayer.SetActive(true);
             opponentPlayer.SetActive(false);
-            Debug.Log("myplayer Parent => " + myPlayer.transform.parent.name);
-            camera.transform.SetParent(myPlayer.transform.parent.transform);
-            camera.transform.position = Vector3.zero;
+            camera.gameObject.SetActive(true);
         }
         else
         {
             myPlayer.SetActive(false);
             opponentPlayer.SetActive(true);
+            camera.gameObject.SetActive(false);
         }
     }
 }
